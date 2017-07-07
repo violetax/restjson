2ad4fe0d9775dba76b5e74d02134335f0ff6136b
U-ENTELIBAI\vgonzalez <vgonzalez@EI001410.entelibai.local>
2017-07-07 08:23:37 +0200

    pro limpio - con algun comment aun

--- WebContent/resources/js/codigo.js
+++ WebContent/resources/js/codigo.js
@@ -4,26 +4,24 @@ jQuery( document ).ready(function( $ ) {
 	
 	$('#ocultar').hide();
 
+	
+//LEAFLET VARS
+	
+	var myIcon = L.icon({
+	    iconUrl: 'resources/images/solarpanel.png',
+	    iconSize: [20, 48],
+	    iconAnchor: [22, 94],
+	    popupAnchor: [-3, -76],
+	});
 //DIBUJAR MAPA//////////////////////////
-var mymap = L.map('mapid').setView([43.264432, -2.92686], 15);
+var mymap = L.map('mapid').setView([42.994603451901334, -2.4238586425781254], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 12,
+	maxZoom: 9,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	id: 'mapbox.streets'
 }).addTo(mymap);
-L.marker([43.264432, -2.92686]).addTo(mymap);
-
-/*
-var popup = L.popup();
-function onMapClick(e) {
-	popup
-		.setLatLng(e.latlng)
-		.setContent("You clicked the map at " + e.latlng.toString())
-		.openOn(mymap);
-}
-mymap.on('click', onMapClick); */
 
 
 //DIBUJAR UN PUNTO///////////////////////////////////////////
@@ -32,13 +30,12 @@ var jsonCoords = "";
 var clickCircle;
 var clickCircle2;
 
+//Recoger las coordenadas de un punto/////////////////////////////////////
+//Crear variable string con forma de json con las coordenadas de un punto
 function getCoordinates(ev) {
    lat = ev.latlng.lat.toString();
    lng = ev.latlng.lng.toString();
-
-   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-   jsonCoords =  JSON.stringify({ "Latitud": lat , "Longitud": lng }); //"{\"Latitud\": " + lat + ", \"Longitud\": " + lng + "}"; 
-   
+   jsonCoords = JSON.stringify({ "Latitud": lat , "Longitud": lng }); 
    
    if (clickCircle != undefined) {
 	   mymap.removeLayer(clickCircle);
@@ -49,20 +46,24 @@ function getCoordinates(ev) {
 	       	fillOpacity: 0.5
 	       }).addTo(mymap);
 }
-
 mymap.on('click', getCoordinates);
+//MOSTRAR LAS COORDENADAS DEL PUNTO EN PANTALLA///////////////
+$("#boton_jcoord").on("click",function(e){	
+	$('#ocultar').show();
+	$('#ocultar').html(jsonCoords);	
+ });
+/////////////////////////////////////////////////////////////////////////////
 
-///************************-PENDIENTE-********************************//////////
-//EXTRAER COORDENADAS DE VARIOS PUNTOS//////////////////////////////////////////
-/*
- * <p>Recoger coordenadas de varios punto</p>
-		<button id="boton_recoger" type="button">RECOGER</button>
- * 
+
+//Recoger las coordenadas de varios puntos//////////////////
+//Crear variable string con forma de json con las coordenadas de varios puntos
+
 var arr = [];
+
 function getCoordinatesBunch(ev) {
 	   lat = ev.latlng.lat.toString();
 	   lng = ev.latlng.lng.toString();
-	   jsonCoords =  JSON.stringify({ "Latitud": lat , "Longitud": lng }); //"{\"Latitud\": " + lat + ", \"Longitud\": " + lng + "}"; 
+	   jsonCoords =  JSON.stringify({ "Latitud": lat , "Longitud": lng }); 
 	   
 		    clickCircle2 = L.circle([lat, lng], 500, {
 		       	color: 'green',
@@ -71,53 +72,51 @@ function getCoordinatesBunch(ev) {
 		       }).addTo(mymap);
 	}
 
-mymap.on('click', getCoordinatesBunch);
-//MOSTRAR LAS COORDENADAS TODOS LOS PUNTOS///////////////
-$("#boton_recoger").on("click",function(e){		    
-    i = 0;
-    $('#boton_recoger').each(function()
-    {
-            arr[i++] = jsonCoords;
-    });
-    
-	$.each(arr, function(index, val) {
-	    console.log(val.Latitud);
-	});
+mymap.on('contextmenu', function(ev){
+	   lat = ev.latlng.lat.toString();
+	   lng = ev.latlng.lng.toString();
+	   jsonCoords =  JSON.stringify({ "Latitud": lat , "Longitud": lng }); 
+	   
+		    clickCircle2 = L.circle([lat, lng], 500, {
+		       	color: 'green',
+		       	fillColor: '#f01',
+		       	fillOpacity: 0.5
+		       }).addTo(mymap);
+	
+		    arr.push(jsonCoords);
+	
+});
+
+/////*****!!!!!!!!PENDIENTE PASAR ESTE ARR A UN REST*********!!!!!!!!!!!!!!
+
+
+//LIMPIAR TODOS LOS MARKERS//////////////////////
+
+/////*****!!!!!!!!PENDIENTE QUE SOLO LIMPIE LOS PUNTOS *********!!!!!!!!!!!!!!
+
+$("#boton_limpiar").on("click",function(e){	
+	mymap.removeLayer(clickCircle2);
+	$(".leaflet-interactive").remove();
  });
- */
-///**********************************************************************************//////////
 
 
-//MOSTRAR LAS COORDENADAS DEL PUNTO EN PANTALLA///////////////
-	$("#boton_jcoord").on("click",function(e){	
-		$('#ocultar').show();
-		$('#ocultar').html(jsonCoords);	
-	 });
+//mymap.on('contextmenu', getCoordinatesBunch);
+/////////////////////////////////////////////////////////////////////////////
 
+///**************************************************************//////////
 
 //MOSTRAR TODOS LOS PUNTOS PREDEFINIDOS///////////////////////////
-	var myIcon = L.icon({
-	    iconUrl: 'resources/images/solarpanel.png',
-	    iconSize: [20, 48],
-	    iconAnchor: [22, 94],
-	    popupAnchor: [-3, -76],
-	});
+$("#boton_cargarTodos").on("click",function(e){
 	
-$("#boton_cargartodos").on("click",function(e){
-	
-	$('#ocultar').show();
-	
+	$('#ocultar').show();	
 	
 	//REST PATH
-	var path = "cargartodos";
-	
+	var path = "cargartodos";	
 	url='http://localhost:8080/aRESTJSON/rest/' + path;
-    //console.log(url);
 
 	$.ajax({
 	    type: 'POST',
 	    url: url,
-	 //   contentType: 'application/json; charset=utf-8',
 	    dataType: 'json',
 	    cache: false,
 	    success: AjaxSucceeded,
@@ -128,37 +127,135 @@ $("#boton_cargartodos").on("click",function(e){
 		
 		for (var x = 0; x < result.length; x++) {	
 			 L.marker([result[x].Latitud, result[x].Longitud], {icon: myIcon}).addTo(mymap);
-			 //console.log(result[x].Latitud);
-			 //console.log(result[x].Longitud);
          }
+	//	$('#ocultar').html(result);
+	}
+	function AjaxFailed(result) {
+		console.log(result);
+	}      
+    });
+
+//////////////POST ******** CARGAR VARIOS PUNTOS *******/////////////////
+$("#boton_registrarPuntos").on("click",function(e){
 		
-/*		
- 	
- 			success: function (data) {
-            alert(data);
-            for (var x = 0; x < data.length; x++) {
-                content = data[x].Id;
-                content += "<br>";
-                content += data[x].Name;
-                content += "<br>";
-                $(content).appendTo("#ProductList");
-               // updateListing(data[x]);
-            }
-        }
-		;
-*/
+	$('#ocultar').show();	
+	var variosPuntos = JSON.stringify(arr);
+	
+	//REST PATH
+	var path = "registrarpuntos";	
+	url='http://localhost:8080/aRESTJSON/rest/' + path;
+
+	$.ajax({
+	    type: 'POST',
+	    url: url,
+	    dataType: 'json',
+	    data: variosPuntos,
+	    cache: false,
+	    success: AjaxSucceeded,
+	    error: AjaxFailed
+	});
+	function AjaxSucceeded(variosPuntos) {
+		console.log(variosPuntos);		
 		
+		//for (var x = 0; x < result.length; x++) {L.marker([result[x].Latitud, result[x].Longitud], {icon: myIcon}).addTo(mymap);}
+	//	$('#ocultar').html(result);
+	}
+	function AjaxFailed(variosPuntos) {
+		console.log(variosPuntos);
+	}      
+    });
+    
+
+//POST*** CARGAR UN PUNTO  ///////////////////////////
+$("#boton_cargarPorPunto").on("click",function(e){
+	
+	$('#ocultar').show();
+	
+	//REST PATH
+	var path = "cargarporpunto";
+	
+	url='http://localhost:8080/aRESTJSON/rest/' + path;
+	
+
+	$.ajax({
+	    type: 'POST',
+	    url: url,
+	    contentType: 'application/json; charset=utf-8',
+	    data: jsonCoords,
+	    dataType: 'json',
+	    cache: false,
+	    success: AjaxSucceeded,
+	    error: AjaxFailed
+	});
+	
+	function AjaxSucceeded(result) {
+		console.log(result.Latitud);
+		console.log(result);
+		$('#ocultar').html(result);
+	}
+	function AjaxFailed(result) {
+		console.log("Fail" + result);
+	}
+	
+    });
+
+
+    /*
+    L.circle([51.508, -0.11], 500, {
+    	color: 'red',
+    	fillColor: '#f03',
+    	fillOpacity: 0.5
+    }).addTo(mymap).bindPopup("I am a circle.");
+
+    L.polygon([
+    	[51.509, -0.08],
+    	[51.503, -0.06],
+    	[51.51, -0.047]
+    ]).addTo(mymap).bindPopup("I am a polygon.");
+    */
+/*
+ * 			<input type="button" value="CARGARfromWS" id="boton_cargarfromWS">
+$("#boton_cargarfromWS").on("click",function(e){
+	
+	$('#ocultar').show();	
+	
+	//REST PATH
+	url='http://192.168.4.31:3000/api/paneles';
+
+	$.ajax({
+	    type: 'POST',
+	    url: url,
+	    dataType: 'json',
+	    cache: false,
+	    success: AjaxSucceeded,
+	    error: AjaxFailed
+	});
+	function AjaxSucceeded(result) {
+		console.log(result);		
+		
+		for (var x = 0; x < result.length; x++) {	
+			 L.marker([result[x].Latitud, result[x].Longitud], {icon: myIcon}).addTo(mymap);
+         }
 	//	$('#ocultar').html(result);
 	}
 	function AjaxFailed(result) {
 		console.log(result);
 		//console.log(result.status + ' ' + result.statusText);
-	}
-       
+	}      
     });
-
+*/
 
 //POST:POST MOSTRAR INFORMACION DEL PUNTO SELECCIONADO ///////////////////////////
+
+/*
+ * <p>Mostrar alrededor de un punto:</p>
+   <form name="formBuscar" action="#">
+			<input type="button" value="CARGAR COORDS. UN PUNTO" id="boton_cargarporpunto">
+			<input type="number" name="radio" id="radio" placeholder="Radio">
+			<input type="button" value="CARGARxAREA" id="boton_cargarporarea">
+   </form>
+
+
 $("#boton_cargarporarea").on("click",function(e){
 	
 	//Envio un string en imitacion de un JSON con varios ptos
@@ -174,7 +271,7 @@ $("#boton_cargarporarea").on("click",function(e){
 	/* El post pasa la info en el campo data: // radio es la var con la que calculo un circulo, NO lo que le paso.
 	 ******** Paso el conjunto de las coordenadas del borde del circulo ****************
 	 * En el servidor se aplica funcion que compara coordenadas y devuelve las de los puntos dentro del circulo dado
-	 */
+
 	
 	//REST PATH
 	var path = "cargarporarea";
@@ -203,62 +300,7 @@ $("#boton_cargarporarea").on("click",function(e){
        
     });
 
-
-//POST:POSTED*** MOSTRAR INFORMACION DEL PUNTO SELECCIONADO ///////////////////////////
-$("#boton_cargarporpunto").on("click",function(e){
-	
-	//hmmm... Se supone que al pinchar el btn del porst se calculan jsonCoords que es una String
-	//SÍ! LAS LEE: console.log(jsonCoords);
-	//Envio las jcoord como string
-	//Devuelvo el mismo string
-	
-	$('#ocultar').show();
-	
-	//REST PATH
-	var path = "cargarporpunto";
-	
-	url='http://localhost:8080/aRESTJSON/rest/' + path;
-	
-
-	$.ajax({
-	    type: 'POST',
-	    url: url,
-	    contentType: 'application/json; charset=utf-8',
-	    data: jsonCoords,
-	    dataType: 'json',
-	    cache: false,
-	    success: AjaxSucceeded,
-	    error: AjaxFailed
-	});
-	
-	function AjaxSucceeded(result) {
-		console.log(result.Latitud);
-		console.log(result);
-		//console.log(result.status + ' ' + result.statusText);
-	//	var res2 = JSON.parse(result);
-		//console.log(res2);
-		$('#ocultar').html(result);
-	}
-	function AjaxFailed(result) {
-		console.log("Fail" + result);
-		//console.log(result.status + ' ' + result.statusText);
-	}
-	
-    });
-    
-    /*
-    L.circle([51.508, -0.11], 500, {
-    	color: 'red',
-    	fillColor: '#f03',
-    	fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("I am a circle.");
-
-    L.polygon([
-    	[51.509, -0.08],
-    	[51.503, -0.06],
-    	[51.51, -0.047]
-    ]).addTo(mymap).bindPopup("I am a polygon.");
-    */
+*/
 
 
 });
